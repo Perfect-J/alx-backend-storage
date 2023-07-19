008cd91da2ca7c6ea1308858ec1233adb1915840
Johnson Fadogba <johnsonayo532@gmail.com>
2023-07-19 13:55:13 +0100

    Update web.py

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -6,7 +6,8 @@ import requests
 from typing import Callable
 from functools import wraps
 
-redis = redis.Redis()
+# Create a Redis client instance
+redis_client = redis.Redis()
 
 
 def wrap_requests(fn: Callable) -> Callable:
@@ -15,12 +16,16 @@ def wrap_requests(fn: Callable) -> Callable:
     @wraps(fn)
     def wrapper(url):
         """ Wrapper for decorator guy """
-        redis.incr(f"count:{url}")
-        cached_response = redis.get(f"cached:{url}")
+        # Increment the count for this URL in Redis
+        redis_client.incr(f"count:{url}")
+        # Check if the response is cached in Redis
+        cached_response = redis_client.get(f"cached:{url}")
         if cached_response:
             return cached_response.decode('utf-8')
+        # If not cached, fetch the response from the web server
         result = fn(url)
-        redis.setex(f"cached:{url}", 10, result)
+        # Cache the response in Redis with an expiration time of 10 seconds (adjust as needed)
+        redis_client.setex(f"cached:{url}", 10, result)
         return result
 
     return wrapper
@@ -28,7 +33,6 @@ def wrap_requests(fn: Callable) -> Callable:
 
 @wrap_requests
 def get_page(url: str) -> str:
-    """get page self descriptive
-    """
+    """get page self-descriptive"""
     response = requests.get(url)
     return response.text
